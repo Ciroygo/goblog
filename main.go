e28ba904bdd2c2b5748f320be159abaff84ab75e
ciroy <ciroygo@gmail.com>
2021-02-16 17:23:45 +0800

    设置标头

--- main.go
+++ main.go
@@ -6,10 +6,11 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if r.URL.Path == "/" {
 		fmt.Fprintf(w, "hellp. by ciroygo112313")
 	} else if r.URL.Path == "/about" {
-		fmt.Fprintf(w, "about page")
+		fmt.Fprintf(w, "about page <a href=\"http://baidu.com\">点击事件</a>")
 	} else {
 		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
 		"<p>如有疑惑，请联系我们。</p>")
