dd2a8567e3fbac1b6df81e4486c153f74e85cce8
ciroy <ciroygo@gmail.com>
2021-04-13 18:01:35 +0800

    8.4 新建Article模型

--- app/http/controllers/articles_controller.go
+++ app/http/controllers/articles_controller.go
@@ -1,11 +1,12 @@
 package controllers
 
 import (
-	"database/sql"
 	"fmt"
+	"goblog/app/models/article"
 	"goblog/pkg/logger"
 	"goblog/pkg/route"
 	"goblog/pkg/types"
+	"gorm.io/gorm"
 	"html/template"
 	"net/http"
 )
@@ -18,10 +19,10 @@ type ArticlesController struct {
 func (*ArticlesController) Show(w http.ResponseWriter, r *http.Request) {
 	id := route.GetRouteVariable("id", r)
 
-	article, err := getArticleByID(id)
+	article, err := article.Get(id)
 
 	if err != nil {
-		if err == sql.ErrNoRows {
+		if err == gorm.ErrRecordNotFound {
 			w.WriteHeader(http.StatusNotFound)
 			fmt.Fprint(w, "404 文章没有找到")
 		} else {
@@ -30,16 +31,15 @@ func (*ArticlesController) Show(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusInternalServerError)
 			fmt.Fprint(w, "500 服务器内部错误")
 		}
+	} else {
+
+		tmpl, err := template.New("show.gohtml").Funcs(template.FuncMap{
+			"RouteName2URL": route.Name2URL,
+			"Int64ToString": types.Int64ToString,
+		}).ParseFiles("resources/views/articles/show.gohtml")
+
+		logger.LogError(err)
+
+		tmpl.Execute(w, article)
 	}
-
-	//tmpl, err := template.ParseFiles("resources/views/articles/show.gohtml")
-
-	tmpl, err := template.New("show.gohtml").Funcs(template.FuncMap{
-		"RouteName2URL": route.Name2URL,
-		"Int64ToString": types.Int64ToString,
-	}).ParseFiles("resources/views/articles/show.gohtml")
-
-	logger.LogError(err)
-
-	tmpl.Execute(w, article)
 }
