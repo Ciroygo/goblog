6be2fc1ccab1ec2bc55ce22b63693263296ab204
ciroy <ciroygo@gmail.com>
2021-04-14 23:05:16 +0800

    重构中间件

--- main.go
+++ main.go
@@ -1,57 +1,18 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
+	"goblog/app/http/middlewares"
 	"goblog/bootstrap"
-	"goblog/pkg/database"
 	"net/http"
 )
 
 var router = mux.NewRouter()
-var db *sql.DB
-
-func forceHTMLMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// 1. 设置标头
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		// 2. 继续处理请求
-		next.ServeHTTP(w, r)
-	})
-}
-
-func getRouteVariable(parameterName string, r *http.Request) string {
-	vars := mux.Vars(r)
-	return vars[parameterName]
-}
 
 func main() {
-	database.Initialize()
-	db = database.DB
-
 	bootstrap.SetupDB()
 	router = bootstrap.SetupRoute()
 
-	//router.HandleFunc("/articles/{id:[0-9]+}", articlesShowHandler).Methods("GET").Name("articles.show")
-	//router.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
-	//router.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.store")
-	//router.HandleFunc("/articles/create", articlesCreateHandler).Methods("get").Name("articles.create")
-	//router.HandleFunc("/articles/{id:[0-9]+}/edit", articlesEditHandler).Methods("GET").Name("articles.edit")
-	//router.HandleFunc("/articles/{id:[0-9]+}", articlesUpdateHandler).Methods("POST").Name("articles.update")
-	//router.HandleFunc("/articles/{id:[0-9]+}/delete", articlesDeleteHandler).Methods("POST").Name("articles.delete")
-
-	// 自定义 404 页面
-
-	// 中间件：强制内容类型为 HTML
-	router.Use(forceHTMLMiddleware)
-
-	// 通过命名路由获取 URL 示例
-	homeURL, _ := router.Get("home").URL()
-	fmt.Println("homeURL: ", homeURL)
-	articleURL, _ := router.Get("articles.show").URL("id", "23")
-	fmt.Println("articleURL: ", articleURL)
-
-	http.ListenAndServe(":3000", router)
+	http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(router))
 }
