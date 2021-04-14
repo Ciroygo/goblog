ff55b47fe480fc546ea4c1bc9ee3178be96668db
ciroy <ciroygo@gmail.com>
2021-04-14 23:05:16 +0800

    8.9重构中间件

--- routes/web.go
+++ routes/web.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"github.com/gorilla/mux"
 	"goblog/app/http/controllers"
+	"goblog/app/http/middlewares"
 	"net/http"
 )
 
@@ -24,4 +25,5 @@ func RegisterWebRoutes(r *mux.Router)  {
 	r.HandleFunc("/articles/{id:[0-9]+}", ac.Update).Methods("POST").Name("articles.update")
 	r.HandleFunc("/articles/{id:[0-9]+}/delete", ac.Delete).Methods("POST").Name("articles.delete")
 
+	r.Use(middlewares.ForceHTML)
 }
