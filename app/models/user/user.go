a152fd2fe7c1a19b7e2dbaae0925dd96fe580afc
ciroy <ciroygo@gmail.com>
2021-05-25 16:33:04 +0800

    10.6 认证用户

--- app/models/user/user.go
+++ app/models/user/user.go
@@ -1,6 +1,10 @@
 package user
 
-import "goblog/app/models"
+import (
+	"goblog/app/models"
+	"goblog/pkg/model"
+	"goblog/pkg/types"
+)
 
 type User struct {
 	models.BaseModel
@@ -11,4 +15,27 @@ type User struct {
 
 	// gorm:"-" —— 设置 GORM 在读写时略过此字段，仅用于表单验证
 	PasswordConfirm string `gorm:"-" valid:"password_confirm"`
+}
+
+// ComparePassword 对比密码是否匹配
+func (u User) ComparePassword(password string) bool {
+	return u.Password == password
+}
+
+func GetByEmail(email string) (User, error) {
+	var user User
+	if err := model.DB.Where("email = ?", email).First(&user).Error; err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
+
+func Get(idstr string) (User, error) {
+	var user User
+	id := types.StringToInt(idstr)
+	if err := model.DB.First(&user, id).Error; err != nil {
+		return user, err
+	}
+	return user, nil
 }
