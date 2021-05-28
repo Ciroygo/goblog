e6ac7125ac573194b6877be8c52ed18f5f847bc5
ciroy <ciroygo@gmail.com>
2021-05-28 15:49:43 +0800

    11.3 配置和环境变量

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"goblog/app/http/middlewares"
 	"goblog/bootstrap"
+	"goblog/config"
+	c "goblog/pkg/config"
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -11,9 +13,14 @@ import (
 
 var router = mux.NewRouter()
 
+func init() {
+	// 初始化配置信息
+	config.Initialize()
+}
+
 func main() {
 	bootstrap.SetupDB()
 	router = bootstrap.SetupRoute()
 
-	http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(router))
+	http.ListenAndServe(":"+c.GetString("app.port"), middlewares.RemoveTrailingSlash(router))
 }
--- pkg/model/model.go
+++ pkg/model/model.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"fmt"
+	"goblog/pkg/config"
 	"goblog/pkg/logger"
 
 	"gorm.io/driver/mysql"
@@ -12,8 +14,21 @@ var DB *gorm.DB
 func ConnectDB() *gorm.DB {
 	var err error
 
+	var (
+		host     = config.GetString("database.mysql.host")
+		port     = config.GetString("database.mysql.port")
+		database = config.GetString("database.mysql.database")
+		username = config.GetString("database.mysql.username")
+		password = config.GetString("database.mysql.password")
+		charset  = config.GetString("database.mysql.charset")
+	)
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=%t&loc=%s",
+		username, password, host, port, database, charset, true, "Local")
+
 	config := mysql.New(mysql.Config{
-		DSN: "root:own3306@tcp(server.pca7.com:3306)/goblog?charset=utf8&parseTime=True&loc=Local",
+		// DSN: "root:own3306@tcp(server.pca7.com:3306)/goblog?charset=utf8&parseTime=True&loc=Local",
+		DSN: dsn,
 	})
 
 	DB, err = gorm.Open(config, &gorm.Config{})
